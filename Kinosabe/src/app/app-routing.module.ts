dc44535837ff07630d308426f428cf90d495ff9e
Kelly Young <kelly.regina.young@gmail.com>
2019-06-12 16:57:49 -0400

    Successful login routes to home page

--- Kinosabe/src/app/app-routing.module.ts
+++ Kinosabe/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
+  {path: "", component: HomeComponent},
   {path: "login", component: LoginComponent},
-  {path: "welcome", component: WelcomeComponent}
+  {path: "home", component: HomeComponent},
+  {path: "**", component: ErrorComponent}
 ];
 
 @NgModule({
