ffa0ba5c5cbcbecdc4cfeddb3ea510eb1cd53f2c
Kelly Young <kelly.regina.young@gmail.com>
2019-06-12 21:17:00 -0400

    Added Batman (1966)

--- Kinosabe/src/app/search-results/search-results.component.ts
+++ Kinosabe/src/app/search-results/search-results.component.ts
@@ -7,19 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultsComponent implements OnInit {
 
-  movieId: number;
-  movieTitle: string;
-  movieDesc: string;
-  movieRating: number;
-  movieGenre: string;
-  movieComments: [];
+  // movieId: number;
+  // movieTitle: string;
+  // movieYear: number;
+  // movieStarring: [];
+  // movieDesc: string;
+  // movieRating: number;
+  // movieGenre: [];
+  // movieComments: [];
+
+  movieId: 771923;
+  movieTitle: "Batman";
+  movieYear: 1966;
+  movieStarring: ["Adam West", "Burt Ward", "Lee Meriwether", "Cesar Romero", "Burgess Meredith", "Frank Gorshin"];
+  movieDesc: "For The First Time On The Motion Picture Screen In Color! Adam West as Batman and Burt Ward as Robin Together With All Their Fantastic Derring-Do And Their Dastardly Villians, Too!";
+  movieRating: 100;
+  movieGenre: ["comedy", "action", "superhero"];
+  movieComments: ["comment1", "comment2", "comment3"];
 
   result = [
-    this.movieId,
     this.movieTitle,
-    this.movieDesc,
+    this.movieYear,
+    this.movieStarring,
     this.movieRating,
-    this.movieGenre
+    this.movieGenre,
+    this.movieDesc,
+    this.movieComments.length
   ];
   
 
