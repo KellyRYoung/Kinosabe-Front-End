a76e24bf15d43f0c3b6e8296c3476b38e18076a8
Kelly Young <kelly.regina.young@gmail.com>
2019-06-14 16:51:20 -0400

    Finished Kinosabe header (for now)

--- Kinosabe/src/app/header/header.component.ts
+++ Kinosabe/src/app/header/header.component.ts
@@ -6,12 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  
+  siteTitle = document.getElementById('site-title');
   title="Kinosabe";
+  fonts = [
+    "Comfortaa",
+    "Chewy",
+    "Fredericka the Great",
+    "Overlock",
+    "Cabin Sketch",
+    "Londrina Solid",
+    "Happy Monkey",
+    "Chelsea Market",
+    "Pompiere",
+    "Sniglet",
+    "Mirza",
+    "Expletus Sans",
+    "ZCOOL KuaiLe",
+    "Finger Paint",
+    "McLaren",
+    "Averia Libre",
+    "Codystar",
+    "Voces",
+    "Life Savers",
+    "Averia Sans Libre",
+    "Nova Square",
+    "Atma",
+    "Kranky",
+    "Yatra One",
+    "Nova Round",
+    "Gugi",
+    "Flamenco",
+    "Coiny",
+    "Autour One",
+    "Original Surfer",
+    "Kirang Haerang",
+    "Yeon Sung",
+    "Londrina Sketch" 
+  ]
   
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
